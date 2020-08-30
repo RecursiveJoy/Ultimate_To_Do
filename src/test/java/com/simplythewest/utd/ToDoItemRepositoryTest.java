4f0dc8d33e0ccd9815a2e9812cb72162e365945f
Megan West <explosivebloom@gmail.com>
2020-08-30 15:54:53 -0500

    Adding assertj

--- src/test/java/com/simplythewest/utd/ToDoItemRepositoryTest.java
+++ src/test/java/com/simplythewest/utd/ToDoItemRepositoryTest.java
@@ -1,8 +1,12 @@
 package com.simplythewest.utd;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import com.simplythewest.utd.ToDoItem;
+import com.simplythewest.utd.ToDoItemRepository;
 
 @DataJpaTest
 @TestPropertySource(properties = {
@@ -11,10 +15,17 @@ import org.springframework.test.context.TestPropertySource;
 
 class ToDoItemRepositoryTest {
 
+    ToDoItemRepository todorepo1;
+
     @Test
     void createThenSave()
     {
-        ToDoItemRepository.save(new ToDoItem("A description", 3));
+        todorepo1.save(new ToDoItem("first", 0));
+        todorepo1.save(new ToDoItem("second", 1));
+        todorepo1.save(new ToDoItem("third", 2));
+        todorepo1.save(new ToDoItem("fourth", 4));
+        ToDoItem third_item = ToDoItemRepository.findByPriority("2");
+        assertThat(third_item).isNotNull();
     }
 
 }
